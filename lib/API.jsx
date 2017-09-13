a6e0dae98adeed2588cc0e4a3b9e77b0282632c5
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-09-13 16:13:03 -0600

    add API class

---
+++ lib/API.jsx
@@ -0,0 +1,20 @@
+import jenKey from './keys.js';
+
+
+export default class API {
+  constructor(location) {
+    this.location = location;
+    this.url = `http://api.wunderground.com/api/${jenKey.jenKey}/conditions/hourly/forecast10day/q/CA/San_Francisco.json`;
+  }
+  getForecast() {
+    fetch(this.url)
+      .then((response) => response.json()) // Transform the data into json
+      .then((data) => {
+        const location = data.current_observation.display_location.full; // Get the results
+        console.log('location is: ', location);
+        return;
+      }).catch((error) => {
+        console.log('ERROR: ', error);
+      });
+  }
+}
