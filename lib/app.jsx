ffe6f89ba5cf2ad4e9429dee5dd3c4f8927aa42d
David Ryan <djrdev@icloud.com>
2017-09-12 20:24:12 -0600

    closer to render still getting error

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../CSS/styles.css'
 
 function Application() {
   return (
     <div className="weatherly">
       <header className="header">
         <h1>Weathrly</h1>
-        <input className="search"></input>
+        <input className="search">Search</input>
       </header>
 
       <main>
         <h2>Current</h2>
         <hr />
-        <section className="current"></section>
+        <section className="current">98 degrees</section>
         <h2>Forecast</h2>
         <hr />
-        <section className="by-hour"></section>
-        <section className="ten-day"></section>
+        <section className="by-hour">78 degrees</section>
+        <section className="ten-day">50 degrees</section>
       </main>
     </div>
   );
