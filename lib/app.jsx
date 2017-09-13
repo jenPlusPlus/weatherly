5d7b3d60c380492586dc8880493611e216d6e5ec
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-09-13 15:12:06 -0600

    capitalize App.jsx

--- lib/app.jsx
+++ lib/app.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../CSS/styles.css'
 
-function Application() {
+function Header() {
+  return (
+    <header className="header">
+      <h1>Weathrly</h1>
+      <input className="search">Search</input>
+    </header>
+  );
+}
+
+export default function App() {
   return (
     <div className="weatherly">
-      <header className="header">
-        <h1>Weathrly</h1>
-        <input className="search">Search</input>
-      </header>
+      <Header />
 
       <main>
         <h2>Current</h2>
@@ -22,5 +28,3 @@ function Application() {
     </div>
   );
 }
-
-ReactDOM.render(<Application />, document.getElementById('container'));
