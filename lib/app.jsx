caed1be1b51b9eaac319b8f79bbaedea1f4626c7
David Ryan <djrdev@icloud.com>
2017-09-13 15:18:34 -0600

    resolved error

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../CSS/styles.css'
-
-function Header() {
-  return (
-    <header className="header">
-      <h1>Weathrly</h1>
-      <input className="search">Search</input>
-    </header>
-  );
-}
+import Header from './Header';
+import '../CSS/styles.css';
 
 export default function App() {
   return (
     <div className="weatherly">
       <Header />
-
       <main>
         <h2>Current</h2>
         <hr />
