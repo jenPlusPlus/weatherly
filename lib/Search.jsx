94b4e39a9e43e4c7bc8a337193877164d6a1c168
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-09-13 15:36:17 -0600

    add Search function

---
+++ lib/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../CSS/styles.css'
+
+
+export default function Search() {
+  return (
+    <input type="search" className="search" placeholder="Search"></input>
+  );
+}
