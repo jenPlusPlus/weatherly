eb6ca5a5539e9cd76cdd2e75bea0d7f980b5f685
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-09-19 10:02:48 -0600

    put starter code in seven and ten test files :wq
    q

--- test/seven.test.js
+++ test/seven.test.js
@@ -1,19 +1,85 @@
-// import React from 'react';
-// import Seven from '../lib/Seven-Hour';
-// // needs data
-// import { shallow, mount } from 'enzyme';
+import React from 'react';
+import Current from '../lib/Current-Weather';
+import { shallow, mount } from 'enzyme';
 
-// describe('', () => {
-//   it('', () => {
-//     const grid = shallow(
-//       <Grid sightings={ufoData.sightings} />
-//     );
-//
-//     console.log(grid.debug());
-//
-//     const rows = grid.find('Row');
-//
-//     expect(rows.length).toEqual(100)
-//     expect(rows.length).toEqual(ufoData.sightings.length)
-//   })
-// })
+describe('Seven Day component', () => {
+  const currentWeather = {
+      sevenHour: [{
+      hour: '9:00 AM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '58.8',
+    },
+    {
+      hour: '10:00 AM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '70',
+    },
+    {
+      hour: '11:00 AM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '74',
+    },{
+      hour: '12:00 PM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '79',
+    },
+    {
+      hour: '1:00 PM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '81',
+    },
+    {
+      hour: '2:00 PM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '83',
+    },
+    {
+      hour: '3:00 AM',
+      iconURL: 'http://icons.wxug.com/i/c/k/partlycloudy.gif',
+      temp: '84',
+    }]
+  }
+  const shallowCurrent = shallow(<Seven data={currentWeather} />)
+
+  it('should display a city', () => {
+    const city = shallowCurrent.find('.current-city');
+
+    expect(city.text()).toEqual(currentWeather.current.city);
+  });
+
+  it('should display the condition', () => {
+    const condition = shallowCurrent.find('.current-condition');
+
+    expect(condition.text()).toEqual(currentWeather.current.condition);
+  });
+
+  it('should display the day', () => {
+    const day = shallowCurrent.find('.current-day');
+
+    expect(day.text()).toEqual(currentWeather.current.day);
+  });
+
+  it('should display the current temperature', () => {
+    const temp = shallowCurrent.find('.current-temp');
+
+    expect(temp.text()).toEqual(currentWeather.current.temp);
+  });
+
+  it('should display high temp', () => {
+    const high = shallowCurrent.find('.current-high');
+
+    expect(high.text()).toEqual(currentWeather.current.highTemp);
+  });
+
+  it('should display low temp', () => {
+    const low = shallowCurrent.find('.current-low');
+
+    expect(low.text()).toEqual(currentWeather.current.lowTemp);
+  });
+
+  it('should display summary', () => {
+    const summary = shallowCurrent.find('.current-summary');
+
+    expect(summary.text()).toEqual(currentWeather.current.summary);
+  });
+});
